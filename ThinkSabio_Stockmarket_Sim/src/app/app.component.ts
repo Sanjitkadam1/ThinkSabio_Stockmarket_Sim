43d4098afc9105e59372ab9f1ea0b2bdd46af314
Sanjitkadam1 <sanjit95kadam@gmail.com>
2024-08-21 00:12:14 -0500

    Routing Starting

--- ThinkSabio_Stockmarket_Sim/src/app/app.component.ts
+++ ThinkSabio_Stockmarket_Sim/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
--- ThinkSabio_Stockmarket_Sim/src/app/app.routes.ts
+++ ThinkSabio_Stockmarket_Sim/src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ApplicationConfig } from '@angular/core';
 
-export const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home-component', component: HomeComponent },
+  { path: 'login-component', component: LoginComponent },
+];
+
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideRouter(routes)]
+// };
+
